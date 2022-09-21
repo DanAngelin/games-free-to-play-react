5e3eecb3391343e34f5210bc0988417285c0ad02
Dan Angelin <dan.angelin@hotmail.com>
2022-09-21 21:34:36 +0300

    created Layout component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,15 @@ import classes from "./Home.module.css";
 import Games from '../../components/Games/Games';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Layout from '../../components/Layout/Layout';
 
 const Home = () => {
 
   return (
     <Box>
-        <Header />
-        <Games />
-        <Footer />
+        <Layout >
+          <Games />
+        </Layout>
     </Box>
   )
 }
